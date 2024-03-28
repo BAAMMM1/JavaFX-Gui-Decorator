0aefeca87804cb2e757303fda6583fec893f747d
cgraumann <christian.graumann@outlook.de>
2024-03-28 22:18:58 +0100

    remove comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,39 +7,7 @@ import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-// TODO Author-Leiste
 
-/*
-Building Jar
-https://stackoverflow.com/questions/1082580/how-to-build-jars-from-intellij-properly+
-
-Artifacts -> GUIDecorator -> Output Layer -> + -> Directory -> Ressources
-
-Build | Build Artifact | Build
-
-jar ist dann in out -> artifacts -> GUIDecorator_jar
-
-Import in ein anderes Projekt
-
-libs ordner erstellen im root und jar reinkopieren
-
-build.gradle
-
-repositories {
-    flatDir {
-        dirs 'lib'
-    }
-}
-
-dependencies {
-    implementation name: 'GUIDecorator'
-}
-
-Reload all gradle projects
-
-Beim Project dann in der root.fxml
-AnchorPane.bottomAnchor="0.0" AnchorPane.leftAnchor="0.0" AnchorPane.rightAnchor="0.0" AnchorPane.topAnchor="0.0" TODO Im decorator setzen
- */
 public class Main extends Application {
 
     private static final String UNDECORATED_VIEW_PATH = "fxml/UserContentSample.fxml";
